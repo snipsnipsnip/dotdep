76c8ca3190fbb97107f75a2db4fa6895366aa964
snipsnipsnip <snipsnipsnip@users.sourceforge.jp>
2013-10-23 02:46:04 +0900

    added --reduce option which does transitive reduction

--- dotdep.rb
+++ dotdep.rb
@@ -36,6 +36,15 @@ class Dep
         dep.fan_counter = a
       end
       
+      o.on('-r', '--reduce', "increase compaction level a la Graphviz tred (default: #{dep.reduce})") do
+        dep.reduce += 1
+      end
+      
+      o.separator "  level 0: no reduction at all"
+      o.separator "  level 1 (-r): randomly guess and dim unimportant path"
+      o.separator "  level 2 (-rr): randomly guess and ignore unimportant path on layout"
+      o.separator "  level 3 (-rrr): randomly guess and delete unimportant path"
+      
       o.parse!(argv)
       
       if froms.size != tos.size
@@ -53,11 +62,16 @@ class Dep
     dep.run(argv)
   end
   
+  REDUCE_LEVEL_HIGHLIGHT = 1
+  REDUCE_LEVEL_IGNORE = 2
+  REDUCE_LEVEL_DELETE = 3
+  
   attr_accessor :ignore_file_matcher
   attr_accessor :source_code_filters
   attr_accessor :case_sensitive
   attr_accessor :cluster
   attr_accessor :fan_counter
+  attr_accessor :reduce
 
   def initialize
     @io = STDOUT
@@ -66,10 +80,14 @@ class Dep
     @case_sensitive = false
     @cluster = false
     @fan_counter = false
+    @reduce = 0
+    @reduced_links = nil
   end
 
   def run(globs)
     graph = scan(@source_code_filters, @case_sensitive, list(globs, @ignore_file_matcher))
+    
+    tred! graph if @reduce > 0
     
     if @cluster && (clusters = calc_cluster(graph)).size >= 2
       print_cluster(graph, clusters)
@@ -79,6 +97,32 @@ class Dep
   end
   
   private
+  
+  # transitively reduce graph
+  def tred!(graph)
+    marks = Set.new
+    @reduced_links = Set.new
+    graph.to_a.shuffle.each {|name, node| tred_dfs graph, marks, name, node, nil }
+  end
+  
+  def tred_dfs(graph, marks, node_name, node, parent)
+    marks.add node_name
+    
+    graph.each do |from_name, from|
+      if from != parent && from.links.include?(node_name) && marks.include?(from_name)
+        @reduced_links << [from_name, node_name]
+      end
+    end
+    
+    node.links.each do |to_name|
+      if !marks.include?(to_name) && !@reduced_links.include?([node_name, to_name])
+        to = graph[to_name]
+        tred_dfs graph, marks, to_name, to, node
+      end
+    end
+    
+    marks.delete node_name
+  end
   
   def list(globs, ignore)
     globs.map {|g| Dir[g].reject {|x| ignore === x } }.flatten
@@ -191,7 +235,13 @@ class Dep
   end
   
   def print_link(from, to, indent=nil)
-    @io.puts %{#{indent}"#{from}" -> "#{to}";} if from != to
+    return if from == to
+    if @reduce >= REDUCE_LEVEL_HIGHLIGHT && @reduced_links.include?([from, to])
+      return if @reduce >= REDUCE_LEVEL_DELETE
+      @io.puts %{#{indent}"#{from}" -> "#{to}" [color="#3366ff66", style=solid, arrowsize=1, style="setlinewidth(4)"#{', constraint=false' if @reduce >= REDUCE_LEVEL_IGNORE}];}
+    else
+      @io.puts %{#{indent}"#{from}" -> "#{to}";}
+    end
   end
   
   def print_node(graph, node_name, node, indent=nil)
@@ -210,8 +260,9 @@ class Dep
     @io.puts 'digraph {'
     @io.puts '  overlap = false;'
     @io.puts '  rankdir = LR;'
-    @io.puts '  node [style = filled, fontcolor = "#123456", fillcolor = white, fontsize = 30, fontname="Arial, Helvetica"];'
-    @io.puts '  edge [color = "#661122"];'
+    @io.puts '  nodesep = 0.5;'
+    @io.puts '  node [fontsize = 40, style = filled, fontcolor = "#123456", fillcolor = white,fontname="Arial, Helvetica", margin="0.22,0.1"];'
+    @io.puts '  edge [color = "#ff1122dd", arrowsize=2, style="setlinewidth(4)"];'
     @io.puts '  bgcolor = "transparent";'
     
     yield
