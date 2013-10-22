098a6f9b28d8fcac7071da94d8ceb84551373079
snipsnipsnip <snipsnipsnip@users.sourceforge.jp>
2013-10-23 04:03:23 +0900

    sprout DotGraphPrinter

--- dotdep.rb
+++ dotdep.rb
@@ -81,14 +81,9 @@ class Dep
 
   def run(globs)
     graph = scan(@source_code_filters, @case_sensitive, list(globs, @ignore_file_matcher))
-    
-    @tred = Tred.new(graph, @reduce)
-    
-    if @cluster && (clusters = calc_cluster(graph)).size >= 2
-      print_cluster(graph, clusters)
-    else
-      print_flat(graph)
-    end
+    tred = Tred.new(graph, @reduce)
+    printer = DotGraphPrinter.new(@io, @cluster, @fan_counter, tred)
+    printer.print_graph(graph)
   end
   
   private
@@ -136,113 +131,133 @@ class Dep
     calc_label(filepath).downcase.gsub('_', '')
   end
   
-  def calc_cluster(dep)
-    dep.group_by {|k,v| v.cluster }
-  end
+  # print graph in Graphviz DOT format
+  class DotGraphPrinter
+    def initialize(io, cluster, fan_counter, link_decorator)
+      @io = io
+      @cluster = cluster
+      @fan_counter = fan_counter
+      @link_decorator = link_decorator
+    end
   
-  def print_cluster(graph, clusters)
-    links = {}
-    outer_links = Set.new
-    
-    graph.each do |nodename, node|
-      node.links.each do |destname|
-        if clusters[node.cluster].any? {|n, _| n == destname }
-          (links[node.cluster] ||= Set.new) << [nodename, destname]
-        else
-          outer_links << [nodename, destname]
-        end
+    def print_graph(graph)
+      if @cluster && (clusters = calc_cluster(graph)).size >= 2
+        print_cluster(graph, clusters)
+      else
+        print_flat(graph)
       end
     end
     
-    print_digraph do
-      indent = '    '
+    private
     
-      links.each_with_index do |(cluster_name, links), i|
-        print_subgraph(i, cluster_name) do
-          clusters[cluster_name].each do |name, node|
-            print_node(graph, name, node, indent)
+    def calc_cluster(dep)
+      dep.group_by {|k,v| v.cluster }
+    end
+    
+    def print_cluster(graph, clusters)
+      links = {}
+      outer_links = Set.new
+      
+      graph.each do |nodename, node|
+        node.links.each do |destname|
+          if clusters[node.cluster].any? {|n, _| n == destname }
+            (links[node.cluster] ||= Set.new) << [nodename, destname]
+          else
+            outer_links << [nodename, destname]
           end
-          
-          links.each {|from, to| print_link(from, to, indent) }
         end
       end
       
-      indent = '  '      
-      outer_links.each {|from, to| print_link(from, to, indent) }
+      print_digraph do
+        indent = '    '
+      
+        links.each_with_index do |(cluster_name, links), i|
+          print_subgraph(i, cluster_name) do
+            clusters[cluster_name].each do |name, node|
+              print_node(graph, name, node, indent)
+            end
+            
+            links.each {|from, to| print_link(from, to, indent) }
+          end
+        end
+        
+        indent = '  '      
+        outer_links.each {|from, to| print_link(from, to, indent) }
+      end
     end
-  end
 
-  def print_flat(dep)
-    print_digraph do
-      @io.puts
-      @io.puts '  // nodes'
-      indent = '  '
-      
-      dep.each do |node_name, node|
-        print_node(dep, node_name, node, indent)
-      end
-      
-      @io.puts
-      @io.puts '  // links'
-      
-      dep.each do |s, node|
-        node.links.each do |d|
-          print_link s, d, indent
+    def print_flat(dep)
+      print_digraph do
+        @io.puts
+        @io.puts '  // nodes'
+        indent = '  '
+        
+        dep.each do |node_name, node|
+          print_node(dep, node_name, node, indent)
+        end
+        
+        @io.puts
+        @io.puts '  // links'
+        
+        dep.each do |s, node|
+          node.links.each do |d|
+            print_link s, d, indent
+          end
         end
       end
     end
-  end
-  
-  def print_subgraph(number, label)
-    @io.puts "  subgraph cluster#{number} {"
-    @io.puts "    label = #{label.inspect};"
-    @io.puts %{   fontcolor = "#123456"; fontsize = 30; fontname="Arial, Helvetica";}
     
-    yield
-    
-    @io.puts "  }"
-  end
-  
-  def print_link(from, to, indent=nil)
-    return if from == to
-    
-    case @tred.calc_link_style(from, to)
-    when :normal
-      @io.puts %{#{indent}"#{from}" -> "#{to}";}
-    when :dim
-      @io.puts %{#{indent}"#{from}" -> "#{to}" [color="#3366ff66", style=solid, arrowsize=1, style="setlinewidth(4)"];}
-    when :ignore
-      @io.puts %{#{indent}"#{from}" -> "#{to}" [color="#3366ff66", style=solid, arrowsize=1, style="setlinewidth(4)", constraint=false];}
-    when :delete
-      # output nothing
-    else
-      raise "unexpected value from Tred#filter"
+    def print_subgraph(number, label)
+      @io.puts "  subgraph cluster#{number} {"
+      @io.puts "    label = #{label.inspect};"
+      @io.puts %{   fontcolor = "#123456"; fontsize = 30; fontname="Arial, Helvetica";}
+      
+      yield
+      
+      @io.puts "  }"
     end
-  end
-  
-  def print_node(graph, node_name, node, indent=nil)
-    node.files.each {|f| @io.puts %{#{indent}/* #{f} */} }
-    if @fan_counter
-      fan_in = graph.count {|n,d| d.links.include?(node_name) }
-      fan_out = node.links.size
-      @io.puts %{#{indent}"#{node_name}" [label = "#{node.label}|{#{fan_in} in|#{fan_out} out}", shape = Mrecord];}
-    else
-      @io.puts %{#{indent}"#{node_name}" [label = "#{node.label}", shape = ellipse];}
+    
+    def print_link(from, to, indent=nil)
+      return if from == to
+      
+      case @link_decorator.calc_link_style(from, to)
+      when :normal
+        @io.puts %{#{indent}"#{from}" -> "#{to}";}
+      when :dim
+        @io.puts %{#{indent}"#{from}" -> "#{to}" [color="#3366ff66", style=solid, arrowsize=1, style="setlinewidth(4)"];}
+      when :ignore
+        @io.puts %{#{indent}"#{from}" -> "#{to}" [color="#3366ff66", style=solid, arrowsize=1, style="setlinewidth(4)", constraint=false];}
+      when :delete
+        # output nothing
+      else
+        raise "unexpected value from link decorator"
+      end
     end
-  end
-  
-  def print_digraph
-    @io.puts 'digraph {'
-    @io.puts '  overlap = false;'
-    @io.puts '  rankdir = LR;'
-    @io.puts '  nodesep = 0.5;'
-    @io.puts '  node [fontsize = 40, style = filled, fontcolor = "#123456", fillcolor = white,fontname="Arial, Helvetica", margin="0.22,0.1"];'
-    @io.puts '  edge [color = "#ff1122dd", arrowsize=2, style="setlinewidth(4)"];'
-    @io.puts '  bgcolor = "transparent";'
     
-    yield
+    def print_node(graph, node_name, node, indent=nil)
+      node.files.each {|f| @io.puts %{#{indent}/* #{f} */} }
+      if @fan_counter
+        fan_in = graph.count {|n,d| d.links.include?(node_name) }
+        fan_out = node.links.size
+        @io.puts %{#{indent}"#{node_name}" [label = "#{node.label}|{#{fan_in} in|#{fan_out} out}", shape = Mrecord];}
+      else
+        @io.puts %{#{indent}"#{node_name}" [label = "#{node.label}", shape = ellipse];}
+      end
+    end
     
-    @io.puts '}'
+    def print_digraph
+      @io.puts 'digraph {'
+      @io.puts '  overlap = false;'
+      @io.puts '  rankdir = LR;'
+      @io.puts '  nodesep = 0.5;'
+      @io.puts '  node [fontsize = 40, style = filled, fontcolor = "#123456", fillcolor = white,fontname="Arial, Helvetica", margin="0.22,0.1"];'
+      @io.puts '  edge [color = "#ff1122dd", arrowsize=2, style="setlinewidth(4)"];'
+      @io.puts '  bgcolor = "transparent";'
+      
+      yield
+      
+      @io.puts '}'
+    end
   end
   
   # calculates transitive reduction and determine link style
